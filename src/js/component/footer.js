8bdbc7dec1a7e3f2e778c3a5166cde75aac522fa
Jose Farrera <76214819+Jose120296@users.noreply.github.com>
2023-11-30 00:01:14 +0000

    actualizacion footer

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -4,7 +4,10 @@ export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
 			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a>
+			<a
+				href="www.linkedin.com/in/jose-farrera-44723b180/"
+				className="linkedin"
+			>
 				Jose farrera 😎
 			</a>
 		</p>
