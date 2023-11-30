555ffd86210bdc7007ec6988c1c9eacb8cf5d083
Jose Farrera <76214819+Jose120296@users.noreply.github.com>
2023-11-30 00:07:08 +0000

    single

--- src/js/views/single.js
+++ src/js/views/single.js
@@ -1,26 +1,136 @@
 import React, { useState, useEffect, useContext } from "react";
-import PropTypes from "prop-types";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
+import "../../styles/single.css";
 
-export const Single = props => {
-	const { store, actions } = useContext(Context);
-	const params = useParams();
-	return (
-		<div className="jumbotron">
-			<h1 className="display-4">This will show the demo element: {store.demo[params.theid].title}</h1>
+export const Single = () => {
+  const { actions } = useContext(Context);
+  const params = useParams();
+  const [myContact, setMyContact] = useState(null);
+  const [editButton, setEditButton] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [urlphoto, setUrlphoto] = useState("");
 
-			<hr className="my-4" />
+  useEffect(() => {
+    const contactId = params.theid;
+    actions.particularContact(contactId).then((data) => {
+      setMyContact(data);
+      setName(data.full_name);
+      setEmail(data.email);
+      setPhone(data.phone);
+      setUrlphoto(data.address);
+    });
+  }, [params.theid]);
 
-			<Link to="/">
-				<span className="btn btn-primary btn-lg" href="#" role="button">
-					Back home
-				</span>
-			</Link>
-		</div>
-	);
+  if (!myContact) return null;
+
+  return (
+    <>
+      {editButton ? (
+        <div className="singleContainer">
+          <div className="cardContainer">
+            <div className="row1">
+              <div className="col1">
+                <img src={myContact.address} className="imgProfile" />
+              </div>
+              <div className="col2">
+                <h1>
+                  <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="editInput"
+                    style={{ width: "90%" }}
+                  ></input>
+                </h1>
+                <p>
+                  <i class="fa-solid fa-at"></i>
+                  <input
+                    type="text"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="editInput"
+                  ></input>
+                </p>
+                <p>
+                  <i class="fa-solid fa-mobile-screen-button"></i>
+                  <input
+                    type="number"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                    className="editInput"
+                  ></input>
+                </p>
+                <p>
+                  <i class="fa-solid fa-image"></i>
+                  <input
+                    type="text"
+                    value={urlphoto}
+                    onChange={(e) => setUrlphoto(e.target.value)}
+                    className="editInput"
+                  ></input>
+                </p>
+              </div>
+            </div>
+            <div className="row2">
+              <button
+                onClick={() => {
+                  actions.updateContact(
+                    myContact.id,
+                    name,
+                    email,
+                    urlphoto,
+                    phone
+                  );
+                }}
+                className="editButton"
+              >
+                Guardar
+              </button>
+              <Link
+                onClick={() => window.location.reload()}
+                className="backHome"
+              >
+                <i class="fa-solid fa-circle-chevron-left"></i> Back to Profile
+              </Link>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="singleContainer">
+          <div className="cardContainer">
+            <div className="row1">
+              <div className="col1">
+                <img src={myContact.address} className="imgProfile" />
+              </div>
+              <div className="col2">
+                <h1>{myContact.full_name}</h1>
+                <p>
+                  <i class="fa-solid fa-at"></i> {myContact.email}
+                </p>
+                <p>
+                  <i class="fa-solid fa-mobile-screen-button"></i>{" "}
+                  {myContact.phone}
+                </p>
+              </div>
+            </div>
+            <div className="row2">
+              <button
+                onClick={() => setEditButton(!editButton)}
+                className="editButton"
+              >
+                Editar
+              </button>
+              <Link to="/" className="backHome">
+                <i class="fa-solid fa-circle-chevron-left"></i> Back to Contacts
+              </Link>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
-Single.propTypes = {
-	match: PropTypes.object
-};
